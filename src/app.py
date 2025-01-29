80110860a6de005501cb3268fc345a44b38caca4
suzuki-sota-ok <suzuki.sota.ok@mynavi.jp>
2025-01-29 02:44:06 +0000

    feat: batch process

--- src/app.py
+++ src/app.py
@@ -3,6 +3,7 @@ from firebase_admin import credentials, firestore
 import json
 import logging
 
+# Setup logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
@@ -10,30 +11,48 @@ logger.setLevel(logging.INFO)
 cred = credentials.Certificate('secret/secret.json')
 firebase_admin.initialize_app(cred)
 
+# Get user inputs
 input_database_id = input('Enter the database ID: ')
 input_file_name = input('Enter the file name without extension (make sure the file name matches the collection name): ')
 
+# Initialize Firestore client
 db = firestore.client(database_id=input_database_id)
-data_path = "data/{}.json".format(input_file_name)
-logger.info(data_path)
+data_path = f"data/{input_file_name}.json"
+logger.info(f"Data path: {data_path}")
 
 # Load JSON data
 with open(data_path, 'r') as file:
     data = [json.loads(line) for line in file]
 
-transformed_data = {}
-for item in data:
-    key = item['_id']['$oid']
-    value = {k: v for k, v in item.items() if k not in ['__v', '_id']}
-    transformed_data[key] = value
+# Transform data
+transformed_data = {
+    item['_id']['$oid']: {k: v for k, v in item.items() if k not in ['__v', '_id']}
+    for item in data
+}
+
+
+def chunk_data(data, chunk_size):
+    """Yield successive chunks from data."""
+    for i in range(0, len(data), chunk_size):
+        yield data[i:i + chunk_size]
+
+
+def batch_write(data, collection_name, chunk_size=500):
+    """Write data to Firestore in batches."""
+    for chunk in chunk_data(list(data.items()), chunk_size):
+        batch = db.batch()
+        for document_id, document_data in chunk:
+            try:
+                doc_ref = db.collection(collection_name).document(document_id)
+                batch.set(doc_ref, document_data)
+            except Exception as e:
+                logger.error(f'Error setting document {document_id}: {e}')
+        try:
+            batch.commit()
+        except Exception as e:
+            logger.error(f'Error committing batch: {e}')
+    logger.info('Data uploaded successfully')
+
 
 # Upload data to Firestore
-# Set data in each document within the 'data' collection
-try:
-    for document_id, document_data in transformed_data.items():
-        doc_ref = db.collection(input_file_name).document(document_id)
-        doc_ref.set(document_data)
-except Exception as e:
-    logger.error('Error uploading data: {}'.format(e))
-
-logger.info('Data uploaded successfully')
+batch_write(transformed_data, input_file_name)
